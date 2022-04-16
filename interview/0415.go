0cfc21f1564b3ca931811c0ef47e25219e780192
smallfu6 <smallfu666@gmail.com>
2022-04-16 10:57:57 +0800

    add interview

---
+++ interview/0415.go
@@ -0,0 +1,29 @@
+package main
+
+/*
+
+	成都泥丸科技面试
+	1. len(map) 的消耗, sync.Map 实现原理
+
+	2. 函数返回指针
+		函数退出, 栈销毁, 逃逸分析
+
+	3. goroutine 切换的时机, 发生了什么
+
+	4. goroutine 的栈扩容
+
+	5. defer, panic
+
+	6.ring buffer
+
+	7. 快速排序
+
+	8. channel 底层原理
+
+	9. sync.Mutex 实现原理
+
+	10. string 和 []byte 转换的消耗
+
+	11. 数据库的索引
+
+*/
