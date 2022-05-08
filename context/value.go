7d6741316fe50e683da98f32439e69403a14004a
smallfu6 <smallfu666@gmail.com>
2022-05-08 20:54:18 +0800

    context value

--- context/value.go
+++ context/value.go
@@ -36,5 +36,37 @@ package main
 	参数不匹配, 就会从父上下文中查找该键对应的值, 直到某个父上下文中返回 nil
 	或者查找到对应的值.
 
+*/
 
+import (
+	"context"
+	"fmt"
+	"sync"
+)
+
+func worker(ctx context.Context, wg *sync.WaitGroup) {
+	defer wg.Done()
+	// 多个 goroutine  同时读
+	value := ctx.Value("site")
+	fmt.Println(value)
+}
+
+func main() {
+	// 写数据的时候必须由父context创建子context
+	ctx := context.WithValue(context.Background(), "site", "segmentfault")
+
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go worker(ctx, &wg)
+	}
+
+	wg.Wait()
+}
+
+/*
+	可以发现写数据的时候必须创建新的子 context, 是在主 goroutine 中进行的,
+	而读数据是在 worker 中进行的，若读不到则在父 context 中读数据; worker 中
+	传入的 ctx 需要先创建, 可以认为读和写是有前后关系的, 且存在多个 goroutine
+	并发读, 不存在goroutine 并发读写的情况， 所以 context 是并发安全的;
 */
